c321558859b713bea3d06665cd0c6d2827b524fa
“yaoyanglin” <yaoyang@umich.edu>
2019-12-08 01:25:33 -0500

    update forward

--- OttoDiff/forward.py
+++ OttoDiff/forward.py
@@ -1,17 +1,150 @@
 import numpy as np
 
+class AutoDiffFwd:
+    def __init__(self):
+        self.var2idx = {}
+
+    def createVariables(self, names, vals):
+        """Create all variables needed in the following computation
+
+            INPUTS
+            =======
+            names: variables name ['x', 'y']
+            vals: variables value [1, 2]
+
+            RETURNS
+            ========
+            record the relationship between index and var name
+            Returns a list of Variable objects
+
+            EXAMPLES
+            =========
+            >>> autodiff = AutoDiffFwd()
+            >>> x, y = autodiff.createVariables(['x', 'y'], [1, 2])
+            >>> print(x)
+            >>> print(y)
+            val: 1 der: [ 1.  0.]
+            val: 2 der: [ 0.  1.]
+        """
+        list_of_variables = []
+        num_variables = len(vals)
+        for i in range(num_variables):
+            ders = np.zeros(num_variables)
+            ders[i] = 1
+            self.var2idx[names[i]] = i
+            list_of_variables.append(Variable(vals[i], ders))
+        return list_of_variables
+
+    def reset(self):
+        """Clear the variables recorded in order to be reused in new computation
+        """
+        self.var2idx = {}
+        return
+
+    def createVectorFunction(self, list_of_variables):
+        """Combine multiple functions into an vector function
+
+            INPUTS
+            =======
+            list_of_variables: list of functions
+
+            RETURNS
+            ========
+            Returns a new VectorVariable object
+
+            EXAMPLES
+            =========
+            >>> autodiff = AutoDiffFwd()
+            >>> x, y = autodiff.createVariables(['x', 'y'], [1, 2])
+            >>> f1 = x * (y ** 2)
+            >>> f2 = np.sin(x * y)
+            >>> vec_f = autodiff.createVectorFunction([f1, f2])
+            >>> print(vec_f)
+            val: [ 4,  0.909] der: [[ 4    , 4     ]
+                                    [-0.832, -0.416]]
+        """
+        vals = np.array([var.val for var in list_of_variables])
+        jacobian = np.stack([var.der for var in list_of_variables], axis=0)
+        return VectorVariable(vals, jacobian)
+
+    def getVerboseInformation(self, var):
+        """Print verbose information related to given variable
+        """
+        print(var)
+        if isinstance(var.der, (int, float)) or var.der.shape[0] == 1:
+            print("Current function is an univariate function")
+        elif var.der.shape[0] > 1:
+            print("Current function is an multivariate function")
+        if len(var.der.shape) > 1:
+            print("Current function is an vector function")
+        for var_name, i in self.var2idx.items():
+            print("partial derivative with respect of " + var_name + " is " + str(var.der[:,i]))
+
+
 class Variable:
     def __init__(self, val, der=1):
-        '''
-        val: int/float, (or np.array in the future if multiple input allowed)
-            values of variables for differentiation
+        """
+        val: int/float, value of variable for differentiation
         der: int/float or np.array if mutiple variable
              (it is just a scalar for this milestone),
              partial derivatives of variables for differentiation
              each entry represents an partial derivatives (∂u/∂x, ∂u/∂y, ∂u/∂z, ...)
-        '''
+        """
         self.val = val
         self.der = der
+
+    # Comparison Operators
+
+    def __eq__(self, other):
+        """Check if two variables are equal
+
+            INPUTS
+            =======
+            self: Variable
+            other: Variable
+
+            RETURNS
+            ========
+            Returns True if both val and der of two variables are equal.
+            Otherwise False
+
+            EXAMPLES
+            =========
+            >>> x = Variable(2)
+            >>> y = Variable(2)
+            >>> print(x == y)
+            True
+        """
+        try:
+            # two variable would be equal if and only if their val and der are equal
+            return (self.val == other.val and np.array_equal(self.der, other.der))
+        except:
+            # also return False if other is not a valid Variable
+            return False
+
+    def __ne__(self, other):
+        """Check if two variables are equal
+
+            INPUTS
+            =======
+            self: Variable
+            other: Variable
+
+            RETURNS
+            ========
+            Returns False if both val and der of two variables are equal.
+            Otherwise True
+
+            EXAMPLES
+            =========
+            >>> x = Variable(2)
+            >>> y = Variable(2)
+            >>> print(x != y)
+            False
+        """
+        return not (self == other)
+
+    # Basic Operations
 
     def __add__(self, other):
         """Returns self + other.
@@ -315,6 +448,8 @@ class Variable:
         """
         return Variable(self.val, self.der) ** 0.5
 
+    # Exponentials
+
     def __rpow__(self, base):
         """Returns base^(self) (a^x).
 
@@ -359,6 +494,8 @@ class Variable:
         """
         return Variable(np.exp(self.val), np.exp(self.val) * self.der)
 
+    # Logarithms
+
     def log(self):
         """Returns ln(self) (ln(x)).
 
@@ -379,6 +516,57 @@ class Variable:
             Variable(0.693, 0.5)
         """
         return Variable(np.log(self.val), 1 / (self.val) * (self.der))
+
+    def logarithm(self, a):
+        """Returns log_a(self) (log_a(x)).
+
+            INPUTS
+            =======
+            self: Variable object
+            a : log base
+
+            RETURNS
+            ========
+            a new Variable represents log_a(x)
+            new val = log_a(self.val)
+            new der =  log_a'(x) = 1/(x * log_a(x)) * x'
+
+            EXAMPLES
+            =========
+            >>> x = Variable(2)
+            >>> y = x.logarithm(2)
+            Variable(1, 0.72134)
+        """
+        if a == 1 or a <= 0:
+            raise ValueError("The base should be any positive value != 1")
+        else:
+            return Variable(np.log(self.val) / np.log(a), 1 / (self.val * np.log(a)) * self.der)
+
+    # Logistic function
+
+    def logistic(self):
+        """Returns logistic(self).
+
+            INPUTS
+            =======
+            self: Variable object
+
+            RETURNS
+            ========
+            a new Variable represents g(x)
+            new val = g(x) = 1 / (1 + np.exp(-self.val))
+            new der =  g(x) (1 - g(x)) * x'
+
+            EXAMPLES
+            =========
+            >>> x = Variable(2)
+            >>> y = x.logistic()
+            Variable(0.88, 0.104)
+        """
+        g_x = 1 / (1 + np.exp(-self.val))
+        return Variable(g_x, (1 - g_x) * g_x * self.der)
+
+    # Trig functions
 
     def sin(self):
         """Returns sin(self) (sin(x)).
@@ -444,6 +632,8 @@ class Variable:
         if self.val % np.pi == np.pi / 2:
             raise ValueError("input of tan should not be value of k * pi + pi/2")
         return Variable(np.tan(self.val), 1 / (np.cos(self.val)) ** 2 * self.der)
+
+    # Inverse trig functions
 
     def arcsin(self):
         """Returns arcsin(self) (arcsin(x)).
@@ -514,6 +704,71 @@ class Variable:
         """
         return Variable(np.arctan(self.val), 1/(1 + self.val**2) * self.der)
 
+    # Hyperbolic functions
+
+    def sinh(self):
+        """Returns sinh(self).
+
+            INPUTS
+            =======
+            self: Variable object
+
+            RETURNS
+            ========
+            a new Variable represents sinh(x)
+            new val = (e^(x) - e^(-x)) / 2
+            new der = cosh(x) * x'
+
+            EXAMPLES
+            =========
+            >>> x = Variable(2)
+            >>> y = np.sinh(x)
+            Variable(3.6268, 3.762)
+        """
+        return Variable((np.exp(self.val) - np.exp(-self.val)) / 2, np.cosh(self.val) * self.der)
+
+    def cosh(self):
+        """Returns cosh(self).
+
+            INPUTS
+            =======
+            self: Variable object
+
+            RETURNS
+            ========
+            a new Variable represents cosh(x)
+            new val = (e^(x) + e^(-x)) / 2
+            new der = sinh(x) * x'
+
+            EXAMPLES
+            =========
+            >>> x = Variable(2)
+            >>> y = np.cosh(x)
+            Variable(3.762, 3.626)
+        """
+        return Variable((np.exp(self.val) + np.exp(-self.val)) / 2, np.sinh(self.val) * self.der)
+
+    def tanh(self):
+        """Returns tanh(self).
+
+            INPUTS
+            =======
+            self: Variable object
+
+            RETURNS
+            ========
+            a new Variable represents tanh(x)
+            new val = sinh(x) / cosh(x)
+            new der = 1 / cosh(x)^2 * x'
+
+            EXAMPLES
+            =========
+            >>> x = Variable(2)
+            >>> y = np.tanh(x)
+            Variable(0.964, 0.0706)
+        """
+        return Variable(np.sinh(self.val) / np.cosh(self.val), 1 / (np.cosh(self.val) ** 2) * self.der)
+
     def __str__(self):
         """Returns a string shows the val and der of the current Variable.
 
@@ -533,10 +788,42 @@ class Variable:
         """
         return "val: " + str(self.val) + " der: " + str(self.der)
 
+class VectorVariable(Variable):
+    def __init__(self, vals, jacobian):
+        """
+        val: np.array, values of variable for differentiation
+        der: np.array (Jacobian matrix)
+             matrix of partial derivatives of variables for differentiation
+             each row represents an partial derivatives (∂f1/∂x, ∂f1/∂y, ∂f1/∂z, ...)
+        """
+        self.val = vals
+        self.der = jacobian
+
     def get_jacobian(self):
-        '''
+        """
             Get the Jacobian matrix of partial derivatives.
-            For this milestone, the Jacobian will just be a scalar.
-            Will support muti input function in the future (would get a matrix)
-        '''
+        """
         return self.der
+
+    # VectorVariable would only support scalar operation inherited from Variable
+
+    def __eq__(self, other):
+        """Check if two variables are equal
+        """
+        try:
+            # two variable would be equal if and only if their val and der are equal
+            return (np.array_equal(self.val, other.val) and np.array_equal(self.der, other.der))
+        except:
+            # also return False if other is not a valid Variable
+            return False
+
+    def __ne__(self, other):
+        """Check if two variables are equal
+        """
+        return not (self == other)
+
+    def __str__(self):
+        """Returns a string shows the vals and jacobian matrix of the current vector Variable.
+        """
+        return "vals: " + str(self.val) + " jacobian: " + str(self.der)
+
--- OttoDiff/tests/test_forward.py
+++ OttoDiff/tests/test_forward.py
@@ -4,7 +4,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import pytest
-from forward.forward import *
+from forward import *
 import numpy as np
 
 def test_add_objects():
